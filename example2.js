43d281236ccf8191379bead0d69142afa70ee5ad
ElenaGQA <elena.grebeniuk.qa@gmail.com>
2024-03-04 20:46:15 -0500

    Changes to be committed: 	modified:   example2.js

--- example2.js
+++ example2.js
@@ -1,9 +1,5 @@
 
-<<<<<<< HEAD
 console.log(multiplyTwoNumbers(2,2));
-=======
-console.log(multiplyTwoNumbers(40,2));
->>>>>>> 3087d2c905afc6891e04cfc00b94aee4bd89980a
 console.log(multiplyTwoNumbers(11,2));
 console.log(multiplyTwoNumbers(2,23));
 console.log(multiplyTwoNumbers(2,3));
