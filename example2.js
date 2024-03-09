9101e4b129cfe736731580d65cad0760a2c18ce0
ElenaGQA <elena.grebeniuk.qa@gmail.com>
2024-03-09 10:47:34 -0500

    Changes to be committed: 	modified:   example2.js

--- example2.js
+++ example2.js
@@ -1,8 +1,8 @@
 
-console.log(multiplyTwoNumbers(200,2));
+console.log(multiplyTwoNumbers(151,2));
 console.log(multiplyTwoNumbers(11,2));
 console.log(multiplyTwoNumbers(2,23));
-console.log(multiplyTwoNumbers(2,3));
+console.log(multiplyTwoNumbers(6,3));
 console.log(multiplyTwoNumbers(45,2));
 
 function multiplyTwoNumbers(numberOne, numberTwo){
