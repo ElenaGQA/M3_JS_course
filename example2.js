c022a31746b7074fc6c6eeeef3e7897350919a45
ElenaGQA <elena.grebeniuk.qa@gmail.com>
2024-03-09 10:10:43 -0500

    test1local

--- example2.js
+++ example2.js
@@ -1,8 +1,4 @@
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 94f7062682ae2cdf92ffd198d3b6322c3c057ca2
 console.log(multiplyTwoNumbers(2,2));
 console.log(multiplyTwoNumbers(11,2));
 console.log(multiplyTwoNumbers(2,23));
